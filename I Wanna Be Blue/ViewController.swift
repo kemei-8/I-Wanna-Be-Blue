48da9d0335ee3031350f4599ff72aff33ddd703a
kemei-8 <84368186+kemei-8@users.noreply.github.com>
2021-07-20 01:30:36 -0500

    Update to "I Wanna Be Blue"

--- I Wanna Be Blue/ViewController.swift
+++ I Wanna Be Blue/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var colorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        view.backgroundColor = .blue
+        colorLabel.text = "It's not easy being green."
+        colorLabel.textColor = .green
+    }
+    
 }
 
